0a12b739b3dfdac6ae56367b51a168b657e0d353
Zhmot666 <mailboxforspam@tut.by>
2024-03-07 00:10:02 +0300

    привел в соответствии с PEP

---
+++ API_CLI/data_client.py
@@ -0,0 +1,81 @@
+from abc import ABC, abstractmethod
+import sqlite3
+import psycopg2
+
+
+class DB(ABC):
+
+    @abstractmethod
+    def select(self):
+        pass
+
+    @abstractmethod
+    def insert(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def update(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def delete(self, **kwargs):
+        pass
+
+
+class Sqlite(DB):
+    __CONNECTION = sqlite3.connect('db_sqlite.db')
+    __CREATE_SCRIPTS = '''CREATE TABLE IF NOT EXISTS person(id_user INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                           login TEXT UNIQUE,
+                                                           password TEXT);'''
+
+    def __init__(self):
+        with self.__CONNECTION as self.sql:
+            self.sql.execute(self.__CREATE_SCRIPTS)
+
+    def select(self):
+        with self.__CONNECTION as self.sql:
+            [print(*data) for data in self.sql.cursor().execute('''SELECT * FROM person''').fetchall()]
+
+    # def update(self, id_user, login, password):
+    def update(self, login, password, id_user):
+        with self.__CONNECTION as self.sql:
+            self.sql.execute('''UPDATE person SET login = ?, password = ? WHERE id_user = ?''',
+                             (login, hash(password), id_user))
+
+    def insert(self, login, password):
+        with self.__CONNECTION as self.sql:
+            self.sql.execute('''INSERT INTO person(login, password) VALUES(?, ?)''', (login, hash(password)))
+
+    def delete(self, id_user):
+        with self.__CONNECTION as self.sql:
+            self.sql.execute('''DELETE FROM person WHERE id_user = ?''', (id_user,))
+
+
+class Postgresql(DB):
+    __CONNECTION = psycopg2.connect(host='localhost', port='5432', database='cli_crud', user='postgres',
+                                    password=input('Введите пароль для доступа к базе данных: '))
+    __CREATE_SCRIPTS = '''CREATE TABLE IF NOT EXISTS person(id_user SERIAL PRIMARY KEY,
+                                                           login TEXT UNIQUE,
+                                                           password TEXT)'''
+
+    def __init__(self):
+        with self.__CONNECTION.cursor() as self.sql:
+            self.sql.execute(self.__CREATE_SCRIPTS)
+
+    def select(self):
+        with self.__CONNECTION.cursor() as self.sql:
+            self.sql.execute('''SELECT * FROM person''')
+            [print(*data) for data in self.sql.fetchall()]
+
+    def update(self, id_user, login, password):
+        with self.__CONNECTION.cursor() as self.sql:
+            self.sql.execute('''UPDATE person SET login = %S, password = %s WHERE id_user = %s''',
+                             (login, hash(password), id_user))
+
+    def insert(self, login, password):
+        with self.__CONNECTION.cursor() as self.sql:
+            self.sql.execute('''INSERT INTO person(login, password) VALUES(%s, %s)''', (login, hash(password)))
+
+    def delete(self, id_user):
+        with self.__CONNECTION.cursor() as self.sql:
+            self.sql.execute('''DELETE FROM person WHERE id_user = %s''', (id_user,))
---
+++ API_CLI/main.py
@@ -0,0 +1,50 @@
+from data_client import Sqlite, Postgresql
+
+
+def main():
+    while True:
+        choice_bd = input('''
+Добро пожаловать !!!
+Выберите БД:
+1. Sqlite
+2. Postgresql
+Ваш выбор: ''')
+
+        match choice_bd:
+            case '1':
+                db = Sqlite()
+                break
+            case '2':
+                db = Postgresql()
+                break
+            case _:
+                print('Вы не выбрали БД для подключения')
+
+    while True:
+        choice_menu = input('''
+1. Показать все данные
+2. Добавить данные
+3. Обновить данные
+4. Удалить данные
+5. Выход
+Ваш выбор: ''')
+
+        match choice_menu:
+            case '1':
+                db.select()
+            case '2':
+                db.insert(input('Введите ваш логин: '), input('Введите ваш пароль: '))
+            case '3':
+                db.update(input('Введите ваш id: '), input('Введите ваш логин: '), input('Введите ваш пароль: '))
+            case '4':
+                db.delete(input('Введите ваш id: '))
+            case '5':
+                break
+            case _:
+                print('Такого пункта меню нет')
+
+
+if __name__ == '__main__':
+    main()
+
+# pyinstaller --onefile  main.py
